b769cf3e1b265da6a34c3e5515864f51e91cb66e
vishalsingh <2018uec1001@gmail.com>
2022-04-24 12:34:51 +0530

    Initial Project

---
+++ app/src/main/java/com/coding/codepadlite/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.coding.codepadlite.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Pink200 = Color(1f, 0.6f, 1f, 1f)
+val Pink500 = Color(1f, 0.3f, 1f, 1f)
+val Pink700 = Color(1f, 0f, 1f, 1f)
+val Teal200 = Color(0xFF03DAC5)
+//val Keyword = Color(0xFF0430A8)
+//val Comment = Color(0xFF097E0F)
+//val SemiColon = Color(0xFFA86904)
